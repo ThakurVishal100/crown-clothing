5ebfafa6f9ebcc9f536d1c28da7a99b4778608bb
Vishal Thakur <thakur.vishal1775@gmail.com>
2024-12-24 16:51:09 +0530

    added shoppingcart icon and drpdown acrt functionality

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context.jsx";
+import { CartProvider } from "./contexts/cart.context.jsx";
 // import { Outlet } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
@@ -12,7 +13,9 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,36 +3,37 @@ import { Fragment, useContext } from "react";
 import logo from "../../assets/crown.png";
 import "./navigation.styles.scss";
 import { UserContext } from "../../contexts/user.context";
-import CartIcon from "../../component/cart-icon/cart-icon.component"
+import { CartContext } from "../../contexts/cart.context";
+import CartIcon from "../../component/cart-icon/cart-icon.component";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartDropdown from "../../component/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  
+  const { isCartOpen } = useContext(CartContext);
 
   return (
-
-
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
           <img src={logo} alt="logo" />
-        </Link> 
+        </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
           ) : (
             <Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
           )}
-          <CartIcon/>
+          <CartIcon />
         </div>
-        <CartDropdown/>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
